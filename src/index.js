316e8e382a558e50071104e2d4b65faa97c15ee8
lin <542072149@qq.com>
2018-05-04 17:30:45 +0800

    整合图片fileLoader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import Guide from './guide.png';
 
 function component() {
  var element = document.createElement('div');
  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
  element.classList.add('hello');
+
+ // 将图像添加到现有的div中
+ var myImg = new Image();
+ myImg.src = Guide;
+
+ element.appendChild(myImg);
+
  return element;
 }
 
