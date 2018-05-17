f4f21260e22d690f6461a132fe1b0a6f7da49431
lin <542072149@qq.com>
2018-05-17 15:45:51 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import printMe from './print.js';
 import './style.css';
 import Guide from './guide.png';
 import Data from './data.xml';
@@ -7,6 +8,13 @@ function component() {
  var element = document.createElement('div');
  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
  element.classList.add('hello');
+
+ var btn = document.createElement('button');
+
+ btn.innerHTML = 'click me and check the console!';
+ btn.onclick = printMe;
+
+ element.appendChild(btn);
 
  // 将图像添加到现有的div中
  var myImg = new Image();
